3bdb5c383dd9a306aba0e488a9041758fbc0130c
Mathew Newton <mathew.enewton@gmail.com>
2020-09-26 12:22:17 -0400

    Start initial project

--- src/index.js
+++ src/index.js
@@ -8,7 +8,26 @@ class App extends React.Component {
 
     componenetDidMount () {
         window.navigator.geolocation.getCurrentPosition(
-            
-        )
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
+        );
+    }
+
+    renderContent () {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat} />;
+        }
+
+        return <Spinner message="Please accept location request" />;
+    }
+
+    render() {
+        return <div className="border red">{this.renderContent()}</div>;
     }
 }
+
+ReactDOM.render(<App />, document.querySelector('#root'));
